e673769ee38b1598c9e2a992e8efe9da65e68f1a
akcodz <no7961079@gmail.com>
2026-01-07 22:12:31 +0530

    middleware issue fixed

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,12 +1,12 @@
 import { requireAuth } from "@clerk/express";
 import { User } from "../models/user.model.js";
+import { ENV } from "../config/env.js";
 
 export const protectRoute = [
     requireAuth(),
     async (req, res, next) => {
         try {
             const clerkId = req.auth().userId;
-            console.log(clerkId);
 
             if (!clerkId) {
                 return res.status(401).json({ message: "Unauthorized" });
@@ -17,7 +17,6 @@ export const protectRoute = [
                 return res.status(404).json({ message: "User not found" });
             }
 
-            console.log(user)
             req.user = user;
             next();
         } catch (error) {
